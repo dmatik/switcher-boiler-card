b5e2f45333a61996914f8c762a588af92120d551
dmatik <dmatik@gmail.com>
2024-12-24 11:38:41 +0200

    adjusting design, adding the 3 buttons

--- src/switcher-boiler-card.js
+++ src/switcher-boiler-card.js
@@ -48,9 +48,15 @@ class SwitcherBoilerCard extends LitElement {
     const displayName = name || friendlyName || "Boiler";
     const stateValue = entityState?.state || "Unavailable";
 
+    const displayIcon = icon || entityState?.attributes?.icon || "mdi:waves";
+    
+    const isOn = stateValue === "on";
+    const iconContainerClass = isOn ? "icon-container on" : "icon-container off";
+    const iconClass = isOn ? "icon on" : "icon off";    
+
     let displayState = "";
 
-    if (stateValue === "on") {
+    if (isOn) {
       if (this.config.time_left && !this.config.electrical_current) {
         displayState = this.hass.states[this.config.time_left].state;
       }
@@ -67,42 +73,112 @@ class SwitcherBoilerCard extends LitElement {
       displayState = this.hass.localize(`component.switch.entity_component._.state.off`) || "off";
     }
 
-    const displayIcon = icon || entityState?.attributes?.icon || "mdi:waves";
-    
-    const isOn = stateValue === "on";
-    const iconContainerClass = isOn ? "icon-container on" : "icon-container off";
-    const iconClass = isOn ? "icon on" : "icon off";    
-
     return html`
-      <div class="card" id="card" @click="${this._showMoreInfo}">
-        <div class="${iconContainerClass}" id="icon-container">
-          <ha-icon icon="${displayIcon}" class="${iconClass}" id="icon"></ha-icon>
-        </div>
-        <div class="label">
-          <span class="primary" id="name">${displayName}</span>
-          <span class="secondary" id="state">${displayState}</span>
-        </div>
-      </div>
+      <ha-card class="card" id="card" @click="${this._showMoreInfo}">
+        <div class="container">
+          <div class="content">
+            <div class="${iconContainerClass}" id="icon-container">
+              <ha-icon icon="${displayIcon}" class="${iconClass}" id="icon"></ha-icon>
+            </div>
+            <div class="label">
+              <span class="primary" id="name">${displayName}</span>
+              <span class="secondary" id="state">${displayState}</span>
+            </div>
+          </div>
+          <div class="controls">
+            <div class="buttons-group">
+              <button class="button" @click=${this._showMoreInfo}>D</button>
+              <button class="button" @click=${this._showMoreInfo}>D</button>
+              <button class="button" @click=${this._showMoreInfo}>D</button>
+            <div>
+          </div>
+        </div>      
+      </ha-card>
     `;
   }
   
   static styles = css`
 
-    .card {
+    ha-card {
       display: flex;
       align-items: center;
-      justify-content: flex-start;
-      padding: 9px;
-      background-color: var(--ha-card-background,var(--card-background-color,#fff));
-      border-radius: var(--ha-card-border-radius,12px);
-      box-shadow: var(--ha-card-box-shadow);
+      flex-direction: column;
       cursor: pointer;
-      user-select: none;
-      transition: transform 0.2s;
-      border-width: var(--ha-card-border-width,1px);
-      border-style: solid;
-      border-color: var(--ha-card-border-color,var(--divider-color,#e0e0e0));
+      transition: background-color 0.2s ease;
+      height: 100%;
     }
+
+    // .content {
+    //   display: flex;
+    //   align-items: center;
+    //   justify-content: flex-start;
+    //   padding: 10px;
+    // }
+
+    .container {
+        display: flex;
+        flex-direction: column;
+        flex: 1;
+        width: 100%
+    }   
+
+    .content {
+        position: relative;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        padding: 9px;
+        flex: 1;
+        box-sizing: border-box;
+        pointer-events: none;
+    }    
+
+    .controls {
+        position: relative;
+        display: flex;
+        flex-direction: column;
+        padding: 11px;
+        padding-top: 0px;
+        gap: 12px;
+        width: 100%;
+        //height: 100%;
+        box-sizing: border-box;
+        //justify-content: space-evenly;
+    }
+
+    .buttons-group {
+      display: grid; /* Use a grid layout */
+      grid-template-columns: repeat(3, 1fr); /* Ensure 3 buttons take equal space */
+      row-gap: 0px;
+      column-gap: 12px;
+      width: 100%; /* Make the group take up the full container width */
+    }
+
+    .button {
+      height: var(--feature-height, 42px);
+      border-radius: var(--feature-border-radius, 12px);
+      border: none;
+      background-color: #eeeeee;
+      cursor: pointer;
+      transition: background-color 180ms ease-in-out;
+      text-align: center; /* Center button content */
+    }
+
+    // .card {
+    //   display: flex;
+    //   align-items: center;
+    //   justify-content: flex-start;
+    //   padding: 9px;
+    //   background-color: var(--ha-card-background,var(--card-background-color,#fff));
+    //   border-radius: var(--ha-card-border-radius,12px);
+    //   box-shadow: var(--ha-card-box-shadow);
+    //   cursor: pointer;
+    //   user-select: none;
+    //   transition: transform 0.2s;
+    //   border-width: var(--ha-card-border-width,1px);
+    //   border-style: solid;
+    //   border-color: var(--ha-card-border-color,var(--divider-color,#e0e0e0));   
+    // }
     
     .icon-container {
       display: flex;
@@ -172,7 +248,23 @@ class SwitcherBoilerCard extends LitElement {
       overflow: hidden;
       white-space: nowrap;
       width: 100%;
-    }  
+    }
+    .button {
+      display: block;
+      height: var(--feature-height, 42px);
+      width: 100%;
+      border-radius: var(--feature-border-radius, 12px);
+      border: none;
+      background-color: #eeeeee;
+      cursor: pointer;
+      transition: background-color 180ms ease-in-out;
+    }
+    .button:hover {
+      background-color: #dddddd;
+    }
+    .button:focus {
+      background-color: #cdcdcd;
+    }
   `;
 
   _toggleBoiler() {
@@ -198,6 +290,15 @@ class SwitcherBoilerCard extends LitElement {
   getCardSize() {
     return 3;
   }
+
+  getLayoutOptions() {
+    return {
+      grid_rows: 1,
+      grid_columns: 6,
+      grid_min_rows: 1,
+      grid_max_rows: 2,
+    };
+  }  
 }
 
 const cardDef = {
