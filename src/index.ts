9f12630a2272285179ccd5d3a793e3090f5dd47f
dmatik <dmatik@gmail.com>
2025-04-04 13:47:41 +0300

    bump to v1.2.8

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { SwitcherBoilerCard } from "./card";
 import { SwitcherBoilerCardEditor } from "./editor";
+import { version } from "../package.json"
 
 declare global {
   interface Window {
@@ -7,7 +8,7 @@ declare global {
   }
 }
 
-console.groupCollapsed(`%c SWITCHER-BOILER-CARD %c v${'1.2.7'}`, "color: white; background: #F54436; font-weight: bold", "color: #F54436; font-weight: bold"),
+console.groupCollapsed(`%c SWITCHER-BOILER-CARD %c v${version}`, "color: white; background: #F54436; font-weight: bold", "color: #F54436; font-weight: bold"),
 console.log("Readme:", "https://github.com/dmatik/switcher-boiler-card"),
 console.groupEnd();
 
